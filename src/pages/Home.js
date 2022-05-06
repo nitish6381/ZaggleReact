f7d0f3e87da2fdab99af5b5bdb318cca438fa728
nitish6381 <nitishkumar6381@gmail.com>
2022-05-06 10:53:21 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,6 @@
 const Home = () => {
-<<<<<<< HEAD
     return <h1>Home</h1>;
-    <h1>how r u nitish</h1>
-=======
-    return <h1>you are in Home</h1>;
-    
->>>>>>> cf18de702a9e6aea3a62eddc8fe292661b02c1fb
+
 };
 
 export default Home;
